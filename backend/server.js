1a04846ac6834449689ca56c0888df26f627b699
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-02-16 09:43:39 +0530

    GET_ROUTE

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({
+    message: "Get Goals",
+  });
+});
+
+app.listen(port, () => console.log(`Server is running at ${port}`));
