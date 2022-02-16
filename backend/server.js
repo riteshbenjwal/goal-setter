f990a2cba2566c1a136b993c567951c14b73e5ef
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-02-16 09:57:05 +0530

    BASIC_ROUTING_SETUP

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-
 const port = process.env.PORT || 5000;
 
-const app = express();
+//Import route
 
-app.get("/api/goals", (req, res) => {
-  res.status(200).json({
-    message: "Get Goals",
-  });
-});
+const goalRoutes = require("./routes/goalRoutes");
+
+const app = express();
+app.use("/api/goals", goalRoutes);
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
