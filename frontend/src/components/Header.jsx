c3736a4bc7f7863c520208cad1e8ee4af5f01b3b
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-04-17 18:26:16 +0530

    LOGIN_ROUTE

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
           </Link>
         </li>
         <li>
-          <Link to="/login">
+          <Link to="/register">
             <FaUser /> Register
           </Link>
         </li>
